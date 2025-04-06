60b33b6066b205f56547e637c4a13712f4c4cfc9
FortuneMU2025 <fortune.olose.2022@mumail.ie>
2025-04-06 20:00:32 +0100

    Testing conditional statements in Java

---
+++ .history/java_intro_20250405165105.java
@@ -0,0 +1,12 @@
+public class java_intro {
+    public static void main(String[] args) {
+        int x = 123;
+        double y = 3.14; 
+        String name = "John";
+        boolean isStudent = true;
+        char grade = 'A';
+        byte age = 20;
+        System.out.println(name + "is " + age + " years old and has a grade of " + grade);
+     }
+
+}
